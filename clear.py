dcc8a862fd018f2fe367aa4b9aba8572d70ca5a6
Alexandre Moutinho - RPA Quality <alexandre.moutinho@quality.com.br>
2021-10-07 00:45:11 -0300

    Create Boletim Input
    1) Criado Boletim Input

--- clear.py
+++ clear.py
@@ -1,8 +1,15 @@
 import os
-pasta = 'file:///C:/AutomationEdge/WorkSpace/AE/UserConference'.replace('file:///','')
+
+pasta = r'file:///C:/AutomationEdge/WorkSpace/AE/UserConference'.replace('file:///','')
+
+log_message=''
 delete=[]
+
 files_list =os.listdir(pasta)
+print(files_list)
+
+
 for file in files_list:
     if '.csv' in file:
         delete.append(os.remove(file))
-
+log_message='True'
